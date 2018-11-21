5fafc0cf56f105ae282dbdd753efe1047ee40bc4
AlexGram <agramatik1@gmail.com>
2018-11-21 08:04:32 +0200

    start develop signup component

---
+++ src/components/auth/signup.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import { withRouter } from 'react-router-dom';
+
+class SignUp extends Component {
+    render () {
+        return (
+            <div>SignUp</div>
+        )
+    }
+}
+
+export default SignUp;
