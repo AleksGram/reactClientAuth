a9dd3c25359da4210a6c8c031f58a35ef2eb776f
AlexGram <agramatik1@gmail.com>
2018-11-24 22:01:37 +0200

    check signed in of user

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import reducers from './reducers';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from "./actions/types";
 
 import App from './components/app';
 import Signin from './components/auth/signin';
@@ -12,13 +13,22 @@ import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import RequireAuth from './components/auth/require_auth';
 import Feature from './components/feature';
+import Welcome from './components/welcome';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// If token exist => consider the user to be signed in
+if (token) {
+    store.dispatch({ type: AUTH_USER })
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
       <BrowserRouter >
           <App>
+              <Route exact path="/" component={Welcome}/>
               <Route path="/signin" component={Signin}/>
               <Route path="/signout" component={Signout}/>
               <Route path="/signup" component={Signup}/>
